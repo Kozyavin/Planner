922715d62dcb719e60f28e63404b7381103792bb
Kozyavin V <kioci@yandex.ru>
2023-05-25 21:41:36 +0300

    ChangeTest

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TodosTest {
-    SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");//простая задача
+    SimpleTask simpleTask = new SimpleTask(5, "Позвонить в НетоБанк");//простая задача
 
     String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
     Epic epic = new Epic(55, subtasks);//задача из подзадач
@@ -14,10 +14,10 @@ public class TodosTest {
             "Приложение НетоБанка",
             "Во вторник после обеда"
     );
-
+@BeforeEach
 
     @Test
-    public void PositiveSearchTest() {
+    public void PositiveSearchTest() {     //находим 1-у подходящую по запросу задачу
         TodosManager todos = new TodosManager();
 
         todos.add(simpleTask);
@@ -28,9 +28,8 @@ public class TodosTest {
         Task[] actual = todos.search("Позвонить");
         Assertions.assertArrayEquals(expected, actual);
     }
-
     @Test
-    public void NegativeSearchTest() {
+    public void NegativeSearchTest() {    //проверяем сценарий отсутствия разыскиваемой задачи
         TodosManager todos = new TodosManager();
 
         todos.add(simpleTask);
@@ -39,6 +38,18 @@ public class TodosTest {
 
         Task[] expected = new Task[0];
         Task[] actual = todos.search("У памятника");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void SomeTaskSearchTest() {    //проверяем сценарий нахождения нескольких, подходящих под запрос задач
+        TodosManager todos = new TodosManager();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask,meeting};
+        Task[] actual = todos.search("НетоБанк");
         Assertions.assertArrayEquals(expected, actual);
     }
 
